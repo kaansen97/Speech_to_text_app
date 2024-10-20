f04bd58fbd9f426801514414c0813e065a2b547c
kaansen97 <66313411+kaansen97@users.noreply.github.com>
2024-10-20 11:46:03 +0200

    Update and rename Sesli_Not_Cevirici_windows.bat to Speech_To_Text_windows.bat

--- Speech_To_Text_windows.bat
+++ Speech_To_Text_windows.bat
@@ -1,7 +1,7 @@
 @echo off
 :: Change to the directory where the application is located
 :: Asagiya yazilan yol, uygulamanin bulundugu dizine gore degistirilmelidir.
-cd /d "%UserProfile%\Desktop\TTS_Model"
+cd /d "%UserProfile%\Desktop\Speech_to_text_app"
 
 :: Run the Python script using python3
 python app.py
